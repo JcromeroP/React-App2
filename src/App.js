6f577a99054a3adb10073b8997d837ef5ab5c369
JcromeroP <romeropascuasj@gmail.com>
2024-07-29 21:26:14 -0500

    Project with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function mostrarTitulo(titulo){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>
+      {titulo}
+    </h1>
+  );
+}
+
+function App() {
+
+  const title = 'Fin';
+  return ( 
+    <div>
+    {mostrarTitulo('Hola mundo')}
+    { mostrarTitulo(title)}
     </div>
   );
 }
